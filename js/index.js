b1c196971c5799de1f8669ec990de459b876f438
mrlemons23 <75939501+mrlemons23@users.noreply.github.com>
2020-12-13 19:28:43 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,221 @@
+console.log('working');
+var total=0;
+var i = 0;
+function addItem1(){
+    var table=document.getElementById('tableItems');
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    //var cell3 = row.insertCell(2);
+    var name = document.getElementById('itemName').innerHTML;
+    var price = document.getElementById('price').innerHTML;
+    cell1.innerHTML = name;
+    cell2.innerHTML = price;
+//    cell3.innerHTML = "<button id='myBtn' onclick='removethisrow(this)'>Remove</button>";
+    price=parseInt(price);
+    total = total + price;
+    var output=document.getElementById('totalTable');
+    output.innerHTML=parseInt(total)+'$';
+    i++;
+}
+function addItem2(){
+    var table=document.getElementById('tableItems');
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    //var cell3 = row.insertCell(2);
+    var name = document.getElementById('itemName2').innerHTML;
+    var price = document.getElementById('price2').innerHTML;
+    cell1.innerHTML = name;
+    cell2.innerHTML = price;
+//    cell3.innerHTML = "<button id='myBtn' onclick='removethisrow(this)'>Remove</button>";
+    price=parseInt(price);
+    total = total + price;
+    var output=document.getElementById('totalTable');
+    output.innerHTML=parseInt(total)+'$';
+    i++;
+}
+function addItem3(){
+    var table=document.getElementById('tableItems');
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    //var cell3 = row.insertCell(2);
+    var name = document.getElementById('itemName3').innerHTML;
+    var price = document.getElementById('price3').innerHTML;
+    cell1.innerHTML = name;
+    cell2.innerHTML = price;
+//    cell3.innerHTML = "<button id='myBtn' onclick='removethisrow(this)'>Remove</button>";
+    price=parseInt(price);
+    total = total + price;
+    var output=document.getElementById('totalTable');
+    output.innerHTML=parseInt(total)+'$';
+    i++;
+
+}
+function addItem4(){
+  var table=document.getElementById('tableItems');
+  var row = table.insertRow(1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  //var cell3 = row.insertCell(2);
+  var name = document.getElementById('itemName4').innerHTML;
+  var price = document.getElementById('price4').innerHTML;
+  cell1.innerHTML = name;
+  cell2.innerHTML = price;
+//    cell3.innerHTML = "<button id='myBtn' onclick='removethisrow(this)'>Remove</button>";
+  price=parseInt(price);
+  total = total + price;
+  var output=document.getElementById('totalTable');
+  output.innerHTML=parseInt(total)+'$';
+  i++;
+}
+function addItem5(){
+  var table=document.getElementById('tableItems');
+  var row = table.insertRow(1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  //var cell3 = row.insertCell(2);
+  var name = document.getElementById('itemName5').innerHTML;
+  var price = document.getElementById('price5').innerHTML;
+  cell1.innerHTML = name;
+  cell2.innerHTML = price;
+//    cell3.innerHTML = "<button id='myBtn' onclick='removethisrow(this)'>Remove</button>";
+  price=parseInt(price);
+  total = total + price;
+  var output=document.getElementById('totalTable');
+  output.innerHTML=parseInt(total)+'$';
+  i++;
+}
+function addItem6(){
+  var table=document.getElementById('tableItems');
+  var row = table.insertRow(1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  //var cell3 = row.insertCell(2);
+  var name = document.getElementById('itemName6').innerHTML;
+  var price = document.getElementById('price6').innerHTML;
+  cell1.innerHTML = name;
+  cell2.innerHTML = price;
+//    cell3.innerHTML = "<button id='myBtn' onclick='removethisrow(this)'>Remove</button>";
+  price=parseInt(price);
+  total = total + price;
+  var output=document.getElementById('totalTable');
+  output.innerHTML=parseInt(total)+'$';
+  i++;
+}
+function addItem7(){
+  var table=document.getElementById('tableItems');
+  var row = table.insertRow(1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  //var cell3 = row.insertCell(2);
+  var name = document.getElementById('itemName7').innerHTML;
+  var price = document.getElementById('price7').innerHTML;
+  cell1.innerHTML = name;
+  cell2.innerHTML = price;
+//    cell3.innerHTML = "<button id='myBtn' onclick='removethisrow(this)'>Remove</button>";
+  price=parseInt(price);
+  total = total + price;
+  var output=document.getElementById('totalTable');
+  output.innerHTML=parseInt(total)+'$';
+  i++;
+}
+function addItem8(){
+  var table=document.getElementById('tableItems');
+  var row = table.insertRow(1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  //var cell3 = row.insertCell(2);
+  var name = document.getElementById('itemName8').innerHTML;
+  var price = document.getElementById('price8').innerHTML;
+  cell1.innerHTML = name;
+  cell2.innerHTML = price;
+//    cell3.innerHTML = "<button id='myBtn' onclick='removethisrow(this)'>Remove</button>";
+  price=parseInt(price);
+  total = total + price;
+  var output=document.getElementById('totalTable');
+  output.innerHTML=parseInt(total)+'$';
+  i++;
+}
+function addItem9(){
+  var table=document.getElementById('tableItems');
+  var row = table.insertRow(1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  //var cell3 = row.insertCell(2);
+  var name = document.getElementById('itemName9').innerHTML;
+  var price = document.getElementById('price9').innerHTML;
+  cell1.innerHTML = name;
+  cell2.innerHTML = price;
+//    cell3.innerHTML = "<button id='myBtn' onclick='removethisrow(this)'>Remove</button>";
+  price=parseInt(price);
+  total = total + price;
+  var output=document.getElementById('totalTable');
+  output.innerHTML=parseInt(total)+'$';
+  i++;
+}
+function addItem10(){
+  var table=document.getElementById('tableItems');
+  var row = table.insertRow(1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  //var cell3 = row.insertCell(2);
+  var name = document.getElementById('itemName10').innerHTML;
+  var price = document.getElementById('price10').innerHTML;
+  cell1.innerHTML = name;
+  cell2.innerHTML = price;
+//    cell3.innerHTML = "<button id='myBtn' onclick='removethisrow(this)'>Remove</button>";
+  price=parseInt(price);
+  total = total + price;
+  var output=document.getElementById('totalTable');
+  output.innerHTML=parseInt(total)+'$';
+  i++;
+}
+function addItem11(){
+  var table=document.getElementById('tableItems');
+  var row = table.insertRow(1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  //var cell3 = row.insertCell(2);
+  var name = document.getElementById('itemName11').innerHTML;
+  var price = document.getElementById('price11').innerHTML;
+  cell1.innerHTML = name;
+  cell2.innerHTML = price;
+//    cell3.innerHTML = "<button id='myBtn' onclick='removethisrow(this)'>Remove</button>";
+  price=parseInt(price);
+  total = total + price;
+  var output=document.getElementById('totalTable');
+  output.innerHTML=parseInt(total)+'$';
+  i++;
+}
+function addItem12(){
+  var table=document.getElementById('tableItems');
+  var row = table.insertRow(1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  //var cell3 = row.insertCell(2);
+  var name = document.getElementById('itemName12').innerHTML;
+  var price = document.getElementById('price12').innerHTML;
+  cell1.innerHTML = name;
+  cell2.innerHTML = price;
+//    cell3.innerHTML = "<button id='myBtn' onclick='removethisrow(this)'>Remove</button>";
+  price=parseInt(price);
+  total = total + price;
+  var output=document.getElementById('totalTable');
+  output.innerHTML=parseInt(total)+'$';
+  i++;
+}
+
+function clearCart(){
+    var x = document.getElementById('tableItems');
+    //x.innerHTML=null;
+    for (var j = 0;j<i;j++){
+        document.getElementById("tableItems").deleteRow(1);
+    }
+    total=0;
+    var output=document.getElementById('totalTable');
+    output.innerHTML=parseInt(total);
+    i=0;
+
+}
